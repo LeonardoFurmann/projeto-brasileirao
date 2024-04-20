f94b0769e91ac9954431506b6d29eafe58187d8b
Leonardo Furmann <leohitnerf@gmail.com>
2024-04-20 14:27:59 -0300

    get time por sigla

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.get('/', (request, response) =>  {
     response.send(tabela2024)
-})
+});
+
+app.get('/:sigla', (request, response) => {
+    const sigla = request.params.sigla.toUpperCase();
+    const time = tabela2024.find((time) => time.sigla === sigla);
+    response.send(time)
+});
 
 app.listen(3000, () => console.log('servidor rodando com sucesso'));
